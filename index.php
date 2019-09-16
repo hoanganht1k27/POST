acc1bf55968a6c6b4accda3ca37efab8602ce976
hoanganht1k27 <48499386+hoanganht1k27@users.noreply.github.com>
2019-09-16 10:24:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>POSTER</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  	<link rel="stylesheet" type="text/css" href="css/style.css">
+  	<script type="text/javascript" src="Js/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+	<div class="nav-container">
+		<div class="navigation">
+			<ul class="nav-list">
+				<li>
+					<a href="#" title="Home">
+						<i class="fa fa-home"></i>
+					</a>
+				</li>
+				<li>
+					<a href="#" title="Profile">
+						<i class="fa fa-user"></i>
+					</a>
+				</li>
+				<li>
+					<span>
+						<i class="fa fa-bell"></i>
+					</span>
+				</li>
+			</ul>
+		</div>
+		<div class="logout-container">
+			<a href="#" title="Log out">
+				<i class="fa fa-sign-out"></i>
+			</a>
+		</div>
+	</div>
+	<div class="dropdown-notice">
+		<ul>
+			<li>
+				<a href="#">Nguyen Hoang Anh commented your post</a>
+			</li>
+			<li>
+				<a href="#">Nguyen Hoang Anh commented your post</a>
+			</li>
+			<li>
+				<a href="#">Nguyen Hoang Anh commented your post</a>
+			</li>
+			<li>
+				<a href="#">Nguyen Hoang Anh commented your post</a>
+			</li>
+		</ul>
+	</div>
+	<div class="main">
+		<div class="left-container">
+		</div>
+		<div class="middle-container">
+			<div class="post-yourself-container all-post-hihi">
+				<header>Post something here</header>
+				<form class="post-yourself">
+					<div class="ava-post-yourself">
+						<img src="images/img1.jpg">
+					</div>
+					<input type="text" name="post-yourself" placeholder="What are you thinking?" id="post-yourself">
+					<input type="submit" name="submit-post-yourself" id="submit-post-yourself" value="Post">
+				</form>
+			</div>
+			<div class="all-post">
+				<div class="post all-post-hihi">
+					<div class="post-info">
+						<div class="ava-post">
+							<img src="images/img2.jpg">
+						</div>
+						<div class="author-post">
+							<a href="#">Anh Nguyen</a>
+						</div>
+					</div>
+					<div class="post-content">
+						<p>blaaj jkds sks sks sls  s s s sa s ad fas fs f asdf asdf asf asf sf sd fd df sf sdf sdfsd fs dfs  sfd fsdf sdfsd s dfs dfs </p>
+					</div>
+					<div class="reaction-counter">
+						<p>100 likes</p>
+					</div>
+					<div class="post-reaction">
+						<div class="reaction-option">
+							<ul>
+								<li>
+									<button>
+										<i class="fa fa-thumbs-up"></i>
+									</button>
+								</li>
+								<li>
+									<button>
+										<i class="fa fa-comment"></i>
+									</button>
+								</li>
+							</ul>
+						</div>
+						<div class="comment-container">
+							<div class="comment">
+								<div class="comment-info">
+									<div class="ava-author-comment">
+										<img src="images/img3.jpg">
+									</div>
+									<a href="#">Nguyen Hoang Anh</a>
+								</div>
+								<div class="comment-content">
+									<p>This is so interesting!</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="post all-post-hihi">
+					<div class="post-info">
+						<div class="ava-post">
+							<img src="images/img2.jpg">
+						</div>
+						<div class="author-post">
+							<a href="#">Anh Nguyen</a>
+						</div>
+					</div>
+					<div class="post-content">
+						<p>blaaj jkds sks sks sls  s s s sa s ad fas fs f asdf asdf asf asf sf sd fd df sf sdf sdfsd fs dfs  sfd fsdf sdfsd s dfs dfs </p>
+					</div>
+					<div class="reaction-counter">
+						<p>100 likes</p>
+					</div>
+					<div class="post-reaction">
+						<div class="reaction-option">
+							<ul>
+								<li>
+									<button>
+										<i class="fa fa-thumbs-up"></i>
+									</button>
+								</li>
+								<li>
+									<button>
+										<i class="fa fa-comment"></i>
+									</button>
+								</li>
+							</ul>
+						</div>
+						<div class="comment-container">
+							<div class="comment">
+								<div class="comment-info">
+									<div class="ava-author-comment">
+										<img src="images/img3.jpg">
+									</div>
+									<a href="#">Nguyen Hoang Anh</a>
+								</div>
+								<div class="comment-content">
+									<p>This is so interesting!</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="post all-post-hihi">
+					<div class="post-info">
+						<div class="ava-post">
+							<img src="images/img2.jpg">
+						</div>
+						<div class="author-post">
+							<a href="#">Anh Nguyen</a>
+						</div>
+					</div>
+					<div class="post-content">
+						<p>blaaj jkds sks sks sls  s s s sa s ad fas fs f asdf asdf asf asf sf sd fd df sf sdf sdfsd fs dfs  sfd fsdf sdfsd s dfs dfs </p>
+					</div>
+					<div class="reaction-counter">
+						<p>100 likes</p>
+					</div>
+					<div class="post-reaction">
+						<div class="reaction-option">
+							<ul>
+								<li>
+									<button>
+										<i class="fa fa-thumbs-up"></i>
+									</button>
+								</li>
+								<li>
+									<button>
+										<i class="fa fa-comment"></i>
+									</button>
+								</li>
+							</ul>
+						</div>
+						<div class="comment-container">
+							<div class="comment">
+								<div class="comment-info">
+									<div class="ava-author-comment">
+										<img src="images/img3.jpg">
+									</div>
+									<a href="#">Nguyen Hoang Anh</a>
+								</div>
+								<div class="comment-content">
+									<p>This is so interesting!</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="post all-post-hihi">
+					<div class="post-info">
+						<div class="ava-post">
+							<img src="images/img2.jpg">
+						</div>
+						<div class="author-post">
+							<a href="#">Anh Nguyen</a>
+						</div>
+					</div>
+					<div class="post-content">
+						<p>blaaj jkds sks sks sls  s s s sa s ad fas fs f asdf asdf asf asf sf sd fd df sf sdf sdfsd fs dfs  sfd fsdf sdfsd s dfs dfs </p>
+					</div>
+					<div class="reaction-counter">
+						<p>100 likes</p>
+					</div>
+					<div class="post-reaction">
+						<div class="reaction-option">
+							<ul>
+								<li>
+									<button>
+										<i class="fa fa-thumbs-up"></i>
+									</button>
+								</li>
+								<li>
+									<button>
+										<i class="fa fa-comment"></i>
+									</button>
+								</li>
+							</ul>
+						</div>
+						<div class="comment-container">
+							<div class="comment">
+								<div class="comment-info">
+									<div class="ava-author-comment">
+										<img src="images/img3.jpg">
+									</div>
+									<a href="#">Nguyen Hoang Anh</a>
+								</div>
+								<div class="comment-content">
+									<p>This is so interesting!</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="right">
+			
+		</div>
+	</div>
+</body>
+</html>
